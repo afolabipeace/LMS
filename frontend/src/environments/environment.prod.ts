d6cd700700c2c0a1512a79c64fce11b355ef7791
Afolabi Peace <peaceafolabi04@gmail.com>
2023-03-02 15:29:43 +0100

    updated baseurl

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: "https://peace-lms.000webhostapp.com/",
+  baseUrl: "https://peace-lms.000webhostapp.com",
   fileUrl:"https://peace-lms.000webhostapp.com/uploads/"
 };
